4412b966188d376f5db621e7a37629d992b858be
Sam Joynson <samjoynson@gmail.com>
2016-04-09 17:56:10 -0400

    added import statements to HuffModel

--- src/HuffModel.java
+++ src/HuffModel.java
@@ -1,8 +1,44 @@
 /**
  * Created by samjoynson on 4/9/16.
  */
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.File;
 public class HuffModel implements IHuffModel
 {
-    
 
+
+    @java.lang.Override
+    public void showCodings()
+    {
+
+    }
+
+    @java.lang.Override
+    public void showCounts() {
+
+    }
+
+    @java.lang.Override
+    public void initialize(InputStream stream)
+    {
+        
+
+    }
+
+    @java.lang.Override
+    public void write(InputStream stream, File file, boolean force) {
+
+    }
+
+    @java.lang.Override
+    public void setViewer(HuffViewer viewer) {
+
+    }
+
+    @java.lang.Override
+    public void uncompress(InputStream in, OutputStream out) {
+
+    }
 }
